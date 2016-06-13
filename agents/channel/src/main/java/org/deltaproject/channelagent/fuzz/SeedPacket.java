72ad4a54bc4018997d2542de61a0af99a1e6df9a
root <root@ubuntu>
2016-06-13 02:34:59 -0700

    add

---
+++ agents/channel/src/main/java/org/deltaproject/channelagent/fuzz/SeedPacket.java
@@ -0,0 +1,38 @@
+package org.deltaproject.channelagent.fuzz;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+
+public class SeedPacket {
+	private Map<OFMessage, Integer> map;
+	private OFType type;
+
+	public SeedPacket(OFType t) {
+		this.type = t;
+		map = new LinkedHashMap<OFMessage, Integer>();
+	}
+
+	public OFType getOFType() {
+		return this.type;
+	}
+
+	public void putData(OFMessage m, int len) {
+		// ByteBuf buf = PooledByteBufAllocator.DEFAULT.directBuffer(len);
+		// of.writeTo(buf);
+		//
+		// int length = buf.readableBytes();
+		// byte[] msg = new byte[length];
+		// buf.getBytes(buf.readerIndex(), msg);
+
+		map.put(m, len);
+	}
+
+	public Map<OFMessage, Integer> getMsgMap() {
+		return map;
+	}
+}
