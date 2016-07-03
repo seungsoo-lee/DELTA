197252930996d70c31c4596def987c2179d035fe
seungsoo-lee <seungsoo365@gmail.com>
2016-07-02 22:07:39 -0700

    update

--- agents/host/test-topo-setup.py
+++ agents/host/test-topo-setup.py
@@ -13,7 +13,8 @@ def DeltaNetwork():
 #Add switch
 	s0 = net.addSwitch('s0', dpid='00:00:00:00:00:01')
 	s1 = net.addSwitch('s1', dpid='00:00:00:00:00:02')
-	s2 = net.addSwitch('s2') # for connection with DELTA
+	s2 = net.addSwitch('s2', dpid='00:00:00:00:00:03')
+	s3 = net.addSwitch('s3') # for connection with DELTA
 
 #Add hosts
 	h1 = net.addHost('h1', ip='10.0.0.1')
@@ -21,16 +22,17 @@ def DeltaNetwork():
 
 #Add links
 	net.addLink(s0, h1)
-	net.addLink(s1, h2)
+	net.addLink(s2, h2)
 	net.addLink(s0, s1)
+	net.addLink(s1, s2)
 
-	net.addLink(s2, h1, intfName2='eth1')
+	net.addLink(s3, h1, intfName2='eth1')
 	
 #	net.build()
 	net.start()
 
 #Add hardware interface to switch1 
-	s2.attach('eth1')
+	s3.attach('eth1')
 
 #Set ip
 	h1.cmd("ifconfig eth1 192.168.200.10 netmask 255.255.255.0")
@@ -38,6 +40,7 @@ def DeltaNetwork():
 #connect a controller
 	os.system("sudo ovs-vsctl set-controller s0 tcp:"+sys.argv[1]+":"+sys.argv[2])
 	os.system("sudo ovs-vsctl set-controller s1 tcp:"+sys.argv[1]+":"+sys.argv[2])
+	os.system("sudo ovs-vsctl set-controller s2 tcp:"+sys.argv[1]+":"+sys.argv[2])
 
 	CLI(net)
 	net.stop()
