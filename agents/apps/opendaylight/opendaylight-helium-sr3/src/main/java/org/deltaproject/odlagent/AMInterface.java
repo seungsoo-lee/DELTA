ab75ea53970d3291a5136cd315fdf428940f0cf9
seungsoo-lee <seungsoo365@gmail.com>
2016-10-07 12:08:38 +0900

    onos

--- agents/apps/opendaylight/opendaylight-helium-sr3/src/main/java/org/deltaproject/odlagent/AMInterface.java
+++ agents/apps/opendaylight/opendaylight-helium-sr3/src/main/java/org/deltaproject/odlagent/AMInterface.java
@@ -33,16 +33,22 @@ public class AMInterface extends Thread {
         this.serverIP = "10.0.2.2";
         this.serverPort = 3366;
 
-        String path = ".";
+        String path = "~";
 
-        Properties props = System.getProperties();
-        Enumeration en = props.keys();
-        while (en.hasMoreElements()) {
-            String key = (String) en.nextElement();
+//        Properties props = System.getProperties();
+//        Enumeration en = props.keys();
+//        while (en.hasMoreElements()) {
+//            String key = (String) en.nextElement();
+//
+//            if (key.equals("HOME"))
+//                path = (String) props.get(key);
+//
+//        }
 
-            if (key.equals("HOME"))
-                path = (String) props.get(key);
-
+        String home = System.getenv("HOME");
+        if (home != null) {
+            path = home;
+            System.out.println("MY HOME: "+home);
         }
 
         BufferedReader br = null;
