339a3feeba107a416d27914a8eb8b96c2e877b00
seungsoo-lee <seungsoo365@gmail.com>
2016-11-03 15:24:01 +0900

    update vagrant

---
+++ tools/dev/vagrant/scripts/java8-oracle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2012 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+
+sudo apt-get install software-properties-common -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo apt-get update && sudo apt-get install oracle-java8-installer oracle-java8-set-default git wget -y
+sudo apt-get install ant -y
