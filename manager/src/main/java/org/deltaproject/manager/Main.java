ff1fa6c87257ed8ebc72f417313e9c181ff82662
Jinwoo Kim <jinwoo.kim@kaist.ac.kr>
2017-04-12 20:35:42 +0900

    Fix log directory issue
    PROBLEM: the 'log' directory should be created when the manager initially runs

--- manager/src/main/java/org/deltaproject/manager/Main.java
+++ manager/src/main/java/org/deltaproject/manager/Main.java
@@ -19,7 +19,7 @@ public class Main {
 
         AgentLogger.checkLogDirectory();
         System.setProperty(SimpleLogger.SHOW_SHORT_LOG_NAME_KEY, "TRUE");
-        System.setProperty(SimpleLogger.LOG_FILE_KEY, "log/manager.log");
+        System.setProperty(SimpleLogger.LOG_FILE_KEY, System.getenv("DELTA_ROOT") + "/log/manager.log");
         System.setProperty(SimpleLogger.SHOW_DATE_TIME_KEY, "TRUE");
         System.setProperty(SimpleLogger.DATE_TIME_FORMAT_KEY, "[yyyy.MM.dd HH:mm:ss z]");
 
