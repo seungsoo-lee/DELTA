237165409349f8688599f65c2dd678c3cefd4747
seungsoo-lee <Seungsoo1004!>
2017-07-03 16:47:00 +0900

    Update src

--- agents/apps/opendaylight/opendaylight-carbon/src/main/java/org/deltaproject/odlagent/core/Activator.java
+++ agents/apps/opendaylight/opendaylight-carbon/src/main/java/org/deltaproject/odlagent/core/Activator.java
@@ -7,6 +7,8 @@
  */
 package org.deltaproject.odlagent.core;
 
+import org.apache.felix.dm.Component;
+import org.apache.felix.dm.Dependency;
 import org.apache.felix.dm.DependencyActivatorBase;
 import org.apache.felix.dm.DependencyManager;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
@@ -19,6 +21,9 @@ import org.osgi.framework.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Dictionary;
+import java.util.List;
+
 /**
  * Activator is derived from AbstractBindingAwareConsumer, which takes care
  * of looking up MD-SAL in Service Registry and registering consumer
@@ -29,6 +34,8 @@ public class Activator extends DependencyActivatorBase implements AutoCloseable,
     private static final Logger LOG = LoggerFactory.getLogger(Activator.class);
     private AppAgent appAgent;
     private AMinterface cm;
+    private BundleContext bundleContext;
+    private DependencyManager dependencyManager;
 
     /**
      * Invoked first
@@ -41,20 +48,14 @@ public class Activator extends DependencyActivatorBase implements AutoCloseable,
         BindingAwareBroker broker = bundleContext.getService(brokerRef);
         broker.registerConsumer(this);
 
-        Bundle[] list = bundleContext.getBundles();
+        this.bundleContext = bundleContext;
+        this.dependencyManager = dependencyManager;
 
-//        for (Bundle b : list) {
-//            if (b.getSymbolicName().contains("l2switch")) {
-//                // LOG.info("[DELTA] uninstall! - " + b.getSymbolicName());
-//                // b.uninstall();
-//            }
-//        }
-
-        cm = new AMinterface();
-        cm.setActivator(this);
-        cm.setServerAddr();
-        cm.connectServer("ActAgent");
-        cm.start();
+//        cm = new AMinterface();
+//        cm.setActivator(this);
+//        cm.setServerAddr();
+//        cm.connectServer("ActAgent");
+//        cm.start();
     }
 
     /**
@@ -78,6 +79,8 @@ public class Activator extends DependencyActivatorBase implements AutoCloseable,
         appAgent.setPacketProcessingService(session.getRpcService(PacketProcessingService.class));
         appAgent.setNotificationService(session.getSALService(NotificationService.class));
         appAgent.start();
+
+        testEventListenerUnsubscription("l2switch");
     }
 
     @Override
@@ -91,5 +94,82 @@ public class Activator extends DependencyActivatorBase implements AutoCloseable,
     @Override
     public void destroy(BundleContext bundleContext, DependencyManager dependencyManager) throws Exception {
         LOG.info("[DELTA] destroy() passing");
+    }
+
+    /* 3.1.100: Application Eviction */
+    public String testApplicationEviction(String target) {
+        boolean removed = false;
+
+        Bundle[] list = bundleContext.getBundles();
+
+        for (Bundle b : list) {
+            if (b.getSymbolicName().contains("l2switch")) {
+                LOG.info("[DELTA] uninstall! - " + b.getSymbolicName());
+                try {
+                    b.uninstall();
+                    removed = true;
+                } catch (BundleException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (removed)
+            return "l2switch";
+        else
+            return "null";
+    }
+
+    /* 3.1.090: Event Listener Unsubscription */
+    public String testEventListenerUnsubscription(String input) {
+        String removed = "";
+
+        DependencyManager manager = this.dependencyManager;
+        List<DependencyManager> dlist = manager.getDependencyManagers();
+
+        BundleContext ctx = manager.getBundleContext();
+        Bundle[] blist = ctx.getBundles();
+
+        for (int i = 0; i < dlist.size(); i++) {
+            DependencyManager dm = dlist.get(i);
+            List<Component> temp = dm.getComponents();
+
+            for (int j = 0; j < temp.size(); j++) {
+                Component ct = temp.get(j);
+
+                @SuppressWarnings("unchecked")
+                Dictionary<String, Object> props = ct.getServiceProperties();
+                if (props != null) {
+                    Object res = props.get("salListenerName"); // salListenerName
+                    if (res != null) {
+                        if (!((String) res).contains(input)) {
+                            continue;
+                        }
+
+                        ServiceRegistration sr = ct.getServiceRegistration();
+                        Bundle bd = sr.getReference().getBundle();
+                        LOG.info("[TEST] " + bd.getSymbolicName());
+
+						/* service unregister */
+                        sr.unregister();
+                        removed = sr.toString();
+
+                        String[] lists = sr.getReference().getPropertyKeys();
+                        for (String s : lists) {
+                            LOG.info(s + " ");
+                        }
+
+//                        List<Dependency> dpl = ct.getDependencies();
+//                        for (int k = 0; k < dpl.size(); k++) {
+//                            Dependency dp = dpl.get(k);
+//                            ct.remove(dp);
+//                        }
+
+                        return removed;
+                    }
+                }
+            }
+        }
+        return removed;
     }
 }
