af1017cc1111e2336b284692b6b93e228213d701
seungsoo-lee <seungsoo365@gmail.com>
2017-07-15 14:16:30 +0900

    Update minor codes

--- manager/src/main/java/org/deltaproject/manager/testcase/TestAdvancedCase.java
+++ manager/src/main/java/org/deltaproject/manager/testcase/TestAdvancedCase.java
@@ -149,18 +149,16 @@ public class TestAdvancedCase {
 
     public void initController(boolean switchWait) {
         if (!controllerm.isRunning()) {
-            log.info("Target controller is starting..");
+            log.info("Run target controller..");
             if (controllerm.createController()) {
                 log.info("Target controller: " + controllerm.getType() + " " + controllerm.getVersion());
-                log.info("Target controller setup is completed");
             } else {
                 log.info("Target controller setup is failed");
-                log.info("Terminated the requested test case");
                 return;
             }
 
 			/* waiting for switches */
-            log.info("Listening to switches..");
+            log.info("Listen to switches..");
             controllerm.isConnectedSwitch(switchWait);
             log.info("All switches are connected");
 
