40d0a1e9a66a289d3e571c8a1d74eb7d176b503e
seungsoo-lee <seungsoo365@gmail.com>
2018-06-28 20:30:19 +0900

    Update Main

--- manager/src/main/java/org/deltaproject/manager/Main.java
+++ manager/src/main/java/org/deltaproject/manager/Main.java
@@ -26,7 +26,7 @@ public class Main {
 
     public static void main(String args[]) throws IOException {
         if (args.length < 1) {
-            System.err.println("Usage: bin/run-delta tools/config/<configuration file>");
+            System.err.println("Usage: bin/run-delta <configuration file>");
             System.exit(1);
         }
 
