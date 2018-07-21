4bbcf96c46c5fab2e83cd5289ce2137a77347a82
Jinwoo Kim <jinwoo.kim@kaist.ac.kr>
2018-07-21 12:43:21 +0900

    Temporary resouces for GUI 2

--- manager/src/main/java/org/deltaproject/webui/rest/IframeResource.java
+++ manager/src/main/java/org/deltaproject/webui/rest/IframeResource.java
@@ -1,17 +1,30 @@
 package org.deltaproject.webui.rest;
 
-import org.glassfish.grizzly.http.server.HttpHandler;
-import org.glassfish.grizzly.http.server.Request;
-import org.glassfish.grizzly.http.server.Response;
-
-import javax.xml.ws.spi.http.HttpExchange;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+import static com.google.common.io.ByteStreams.toByteArray;
 
 /**
  * Created by jinwookim on 2018. 7. 20..
  */
+@Path("/new")
 public class IframeResource {
+
+    @GET
+    @Path("/")
+    @Produces(MediaType.TEXT_HTML)
+    public Response getMainPage() throws IOException {
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream mainPage = classLoader.getResourceAsStream("new.html");
+        String strMain = new String(toByteArray(mainPage));
+        InputStream result = new ByteArrayInputStream(strMain.getBytes(StandardCharsets.UTF_8));
+
+        return Response.ok(result).build();
+    }
 }
--- manager/src/main/resources/js/new.js
+++ manager/src/main/resources/js/new.js
@@ -1,6 +1,8 @@
-(function () {
-    var ws = null;
-    ws = new WebSocket("127.0.0.1:7070")
+var ws = null;
+ws = new WebSocket("ws://127.0.0.1:7070")
+var result = ws.send("/new/echo");
 
-    
-}
+var app = angular.module('myApp', []);
+app.controller('myCtrl', function ($scope) {
+    $scope.echo = result;
+});
