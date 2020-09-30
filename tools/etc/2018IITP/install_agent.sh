bd133c26f971cf85da5df41f04d17c9b505b5aee
Seungsoo Lee <seungsoo365@gmail.com>
2020-09-30 13:00:06 +0900

    clean up

--- tools/etc/2018IITP/install_agent.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd ~/DELTA/agents/apps/onos/onos-1.13.1/
-mvn clean install
-onos-app localhost reinstall org.deltaproject.appagent ~/DELTA/agents/apps/onos/onos-1.13.1/target/delta-agent-app-onos-1.13.1-1.0-SNAPSHOT.oar
